90a3a05ccab5cad20b8d8ee508a75fea0781c0a2
Isabel Restrepo <isabelrestre@gmail.com>
2020-08-04 21:31:22 -0400

    test: add rspec-retry to test

--- test/integration/sample-jhub/controls/sample-jhub.rb
+++ test/integration/sample-jhub/controls/sample-jhub.rb
@@ -1,12 +1,13 @@
 # copyright: 2018, The Authors
 
-title "Cluster Section"
+title "Test basic availability of JupyterHub"
+require 'rspec/retry'
 
 jhub_url = attribute("jhub_url")
 
-describe http("https://sample.jupyter.brown.edu/hub/login",
-              ssl_verify: false) do
-    its('status') { should cmp 200 }
-    its('headers.Content-Type') { should cmp 'text/html' }
+describe http("https://sample.jupyter.brown.edu/hub/login", ssl_verify: false) do
+    it 'status should be 200', retry: 300, retry_wait:1 do
+        its('status') { should cmp 200 }
+        its('headers.Content-Type') { should cmp 'text/html' }
+    end
 end
-
