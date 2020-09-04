3c710655d4fc284342e8e2ff6b5c01afcd3f22e1
Isabel Restrepo <isabelrestre@gmail.com>
2020-09-04 16:08:48 -0400

    chore: remove un-used module

--- modules/shared-nfs-old/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "name" {
-  description = "Name of NFS persistent_volume name for shared data location in notebooks"
-  value       = var.use_shared_volume ? kubernetes_persistent_volume.nfs-volume[0].metadata.0.name : ""
-}
