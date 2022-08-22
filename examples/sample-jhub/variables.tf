b50568de06152f8c277429284c6d4637e7d9ef93
Maria Isabel Restrepo <maria_restrepo@brown.edu>
2022-08-22 16:59:21 -0500

    fix: really removing master_authorized_networks

--- examples/sample-jhub/variables.tf
+++ examples/sample-jhub/variables.tf
@@ -14,9 +14,3 @@ variable "site_certificate_file" {
 variable "site_certificate_key_file" {
   default = "./secrets/tls.key"
 }
-
-variable "master_authorized_networks" {
-  type = list(object({ cidr_block = string, display_name = string }))
-  description = "Defines the CIDR blocks that should be able to connect to the cluster control plane. If empty, no external access is possible"
-  default = []
-}
--- variables.tf
+++ variables.tf
@@ -249,11 +249,6 @@ variable "remove_default_node_pool" {
   default     = false
 }
 
-variable "master_authorized_networks" {
-  type = list(object({ cidr_block = string, display_name = string }))
-  description = "Defines the CIDR blocks that should be able to connect to the cluster control plane. If empty, no external access is possible"
-}
-
 # ----------------------------------------
 #  NODE POOL VALUES
 # ----------------------------------------
