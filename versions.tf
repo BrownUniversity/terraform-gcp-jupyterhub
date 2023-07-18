a2074567da75434d829299e27430e4ea940adddd
Paul Xu <yang_xu@brown.edu>
2023-07-18 14:11:44 -0400

    update software versions

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.2.5"
+  required_version = ">= 1.5.0"
 
   required_providers {
-    google      = ">= 4.31.0, <5.0.0"
-    google-beta = ">= 4.31.0, <5.0.0"
-    kubernetes  = ">= 2.12"
-    helm        = ">= 2.6"
+    google      = ">= 4.72.0, <5.0.0"
+    google-beta = ">= 4.72.0, <5.0.0"
+    kubernetes  = ">= 2.22.0"
+    helm        = ">= 2.10.1"
   }
 }
