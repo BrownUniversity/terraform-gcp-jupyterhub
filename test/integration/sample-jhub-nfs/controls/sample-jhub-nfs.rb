be425449ddc603bd649f28bea1286472b9288233
Paul Xu <yang_xu@brown.edu>
2024-07-31 09:56:40 -0400

    remove kitchen tests

--- test/integration/sample-jhub-nfs/controls/sample-jhub-nfs.rb
+++
@@ -1,9 +0,0 @@
-# copyright: 2018, The Authors
-title "Test basic availability of JupyterHub"
-
-jhub_url = attribute("jhub_url")
-
-describe http("#{jhub_url}/hub/login", ssl_verify: false) do
-    its('status') { should cmp 200 }
-    its('headers.Content-Type') { should cmp 'text/html' }
-end
