b25b94f811939b12a0540bd483da95a816a36580
Paul Xu <yang_xu@brown.edu>
2024-08-15 19:11:49 -0400

    try an older version of helm chart

--- tests/sample-jhub/main.tf
+++ tests/sample-jhub/main.tf
@@ -67,7 +67,7 @@ module "sample-jhub" {
   shared_storage_capacity = 2
 
   # ---------------- HELM/JHUB VARIABLES -----------------------
-  jhub_helm_version   = "3.3.8"
+  jhub_helm_version   = "3.2.1"
   helm_deploy_timeout = 1000
   helm_values_file    = var.helm_values_file
 
