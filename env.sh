e6b67b28056707dac687c2b304054f31e9786286
Paul Xu <yang_xu@brown.edu>
2024-08-20 16:22:55 -0400

    update infoblox password reveal

--- env.sh
+++ env.sh
@@ -3,7 +3,7 @@
 export GOOGLE_APPLICATION_CREDENTIALS="$HOME/.gcp/project-factory-gcp.json"
 export USE_GKE_GCLOUD_AUTH_PLUGIN=True
 export INFOBLOX_USERNAME=$(op item get infoblox --field username)
-export INFOBLOX_PASSWORD=$(op item get infoblox --field password)
+export INFOBLOX_PASSWORD=$(op item get infoblox --field password --reveal)
 export INFOBLOX_SERVER=$(op item get infoblox --format json | jq -r '.urls[].href' | awk -F/ '{print $3}')
 
 
