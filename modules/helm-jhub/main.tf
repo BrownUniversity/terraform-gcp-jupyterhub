89661cfe051871c9d219d30b3618dddc7151caaf
Camilo Diaz <k.diaz99@gmail.com>
2025-07-15 13:34:41 -0400

    maint: helm provider needs to declare nested objects

--- modules/helm-jhub/main.tf
+++ modules/helm-jhub/main.tf
@@ -8,7 +8,7 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     cluster_ca_certificate = var.cluster_ca_certificate
     host                   = var.host
     token                  = var.token
